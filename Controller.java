592032961f8015b300304e4b9dc7ebad6c8db410
Max Bretschneider <maxbretschneider@mailbox.org>
2021-06-19 00:22:08 +0200

    initialized controller class

---
+++ Controller.java
@@ -0,0 +1,12 @@
+/*
+* This is the controller class of the BetterMarker program
+* It handles all calculations and util methods.
+* @author Max Bretschneider
+* @author Philipp Richter
+* @version 0.0.1-SNAPSHOT
+*/
+
+public static Class Controller {
+
+    
+}
