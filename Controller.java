8115b079cbba7c6ecb50eda0d880b46d4d0947ac
Max Bretschneider <maxbretschneider@mailbox.org>
2021-06-19 03:22:37 +0200

    Fixed another typo. It is getting late.

--- Controller.java
+++ Controller.java
@@ -113,4 +113,40 @@ public static Class Controller {
         Color color = new Color (image.getColor(x, y));
         return (color == Color.WHITE);
     }
+
+    /**
+    * This test loops over the Dolphins image and
+    * prints out the count of white pixels and of none
+    * white pixels.
+    *
+    */
+    public void testPixelCount(BufferedImage image) {
+        Controller c = new Controller();
+        ScreenImage i = new BufferedScreenImage(image);
+
+        int pixelWhite = 0;
+        int pixelNonWhite = 0;
+
+        ScreenImage toReturn = i.copy();
+
+        //Get height and width of the image
+        int height = toReturn.getHeight();
+        int width = toReturn.getWidth();
+
+        //Loop over the image pixel by pixel
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+
+                //Depending on the defined case (min or max), we set the y-Value to
+                //the new highest/lowest value, if it not white.
+                if (!isColorWhite(x, y, toReturn)) {
+                    pixelNonWhite++;
+                } else if (isColorWhite(x, y, toReturn)) {
+                    pixelWhite++;
+                }
+            }
+        }
+        System.out.println("Pixel count white: " + pixelWhite + ".");
+        System.out.println("Pixel count non-white: " + pixelNonWhite + ".");
+    }
 }
--- Test.java
+++ Test.java
@@ -1,57 +1,21 @@
 import java.awt.image.BufferedImage;
-import java.awt.Color;
-import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Test {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        BufferedImage image = null;
+    BufferedImage image = null;
 
-        try {
-            File imageFile = new File("res/Dolphins.png");
-            image = ImageIO.read(imageFile);
-        } catch (IOException e) {
-            e.prprintStackTrace();
-        }
-
-        /**
-        * This test loops over the Dolphins image and
-        * prints out the count of white pixels and of none
-        * white pixels.
-        *
-        */
-        public void testPixelCount(BufferedImage image) {
-            Controller c = new Controller();
-            ScreenImage i = new BufferedScreenImage(image);
-
-            int pixelWhite = 0;
-            int pixelNonWhite = 0;
-
-            ScreenImage toReturn = i.copy();
-
-            //Get height and width of the image
-            int height = toReturn.getHeight();
-            int width = toReturn.getWidth();
-
-            //Loop over the image pixel by pixel
-            for (int y = 0; y < height; y++) {
-                for (int x = 0; x < width; x++) {
-
-                    //Depending on the defined case (min or max), we set the y-Value to
-                    //the new highest/lowest value, if it not white.
-                    if (!isColorWhite(x, y, toReturn)) {
-                        pixelNonWhite++;
-                    } else if (isColorWhite(x, y, toReturn)) {
-                        pixelWhite++;
-                    }
-                }
-            }
-            System.out.println("Pixel count white: " + pixelWhite + ".");
-            System.out.println("Pixel count non-white: " + pixelNonWhite + ".");
-        }
-
+    try {
+      File imageFile = new File("res/Dolphins.png");
+      image = ImageIO.read(imageFile);
+    } catch (IOException e) {
+      e.prprintStackTrace();
     }
+
+    Controller.testPixelCount(image);
+  }
 }
