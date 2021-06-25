f63516b3fb0e4442d0ebbc98475f8468d084f03e
brtmax <54221876+brtmax@users.noreply.github.com>
2021-06-26 00:50:40 +0200

    Created default methods.

---
+++ org/jis/java/WelcomePanel.java
@@ -0,0 +1,6 @@
+package org.jis.java;
+
+import javax.swing.*;
+
+public class WelcomePanel extends JPanel {
+}
--- org/jis/java/WelcomeUI.java
+++ org/jis/java/WelcomeUI.java
@@ -1,5 +1,52 @@
 package org.jis.java;
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
 
-public class WelcomeUI extends javax.swing.JFrame {
-    
-}
+public class WelcomeUI extends JFrame {
+
+        private JPanel welcomeUIPanel;
+        private JPanel contentPanel;
+        private Controller controller;
+
+        public WelcomeUI(String title) {
+            super(title);
+            this.setLocation(300, 300);
+        }
+
+        public static void main(String[] args) {
+            WelcomeUI backgroundWindow = new WelcomeUI("BetterMarker");
+            backgroundWindow.setVisible(true);
+            backgroundWindow.setupContent();
+        }
+
+        /**
+         * This method sets up the default frame
+         */
+        private void setupContent() {
+            this.setTitle("iGen");
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            //TODO
+            this.setBounds(1, 1, 856, 482);
+            this.setLocationRelativeTo(null);
+            this.contentPanel = new JPanel();
+            this.contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+            this.contentPanel.setLayout(new GridLayout(1, 1));
+            this.setContentPane(this.contentPanel);
+
+            this.generateConent();
+
+            this.setVisible(true);
+
+        }
+
+        /**
+         * This method creates the top panel and adds it to the frame
+         */
+        private void generateConent() {
+            this.welcomeUIPanel = new WelcomePanel();
+            this.contentPanel.add(this.welcomeUIPanel);
+        }
+    }
+
